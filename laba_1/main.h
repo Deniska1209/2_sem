388a4311d77c936e4c800d1daf154b1cee118747
Deniska1209 <mrdenic228@gmail.com>
2025-02-21 11:33:57 +0300

    Update main.h

--- laba_1/main.h
+++ laba_1/main.h
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <locale.h>
 
-// Функция для проверки, является ли число простым
 bool is_prime(int n) {
     if (n <= 1) return false;
     if (n <= 3) return true;
@@ -14,7 +13,6 @@ bool is_prime(int n) {
     return true;
 }
 
-// Функция для вывода массива на экран
 void print_array(int* arr, int size) {
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
@@ -22,14 +20,12 @@ void print_array(int* arr, int size) {
     printf("\n");
 }
 
-// Функция для замены знака каждого числа на противоположный
 void negate_numbers(int* arr, int size) {
     for (int i = 0; i < size; i++) {
         arr[i] = -arr[i];
     }
 }
 
-// Функция для сортировки отрицательных чисел по убыванию
 void sort_negatives(int* arr, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = i + 1; j < size; j++) {
@@ -42,36 +38,33 @@ void sort_negatives(int* arr, int size) {
     }
 }
 
-// Функция для безопасного ввода целого числа
 int safe_input_int(const char* prompt) {
     int value;
     while (1) {
         printf("%s", prompt);
         if (scanf_s("%d", &value) == 1) {
-            break; // Ввод корректен, выходим из цикла
+            break; 
         }
         else {
             printf("Ошибка: введен некорректный символ. Пожалуйста, введите целое число.\n");
-            // Очистка буфера ввода
-            while (getchar() != '\n'); // Удаляем все символы до конца строки
+            while (getchar() != '\n');
         }
     }
     return value;
 }
 
 int main(int argc, char* argv[]) {
-    setlocale(LC_ALL, "Russian"); // Для поддержки русского языка
+    setlocale(LC_ALL, "Russian"); 
 
     if (argc != 2) {
         printf("Нету памяти\n", argv[0]);
         return 1;
     }
 
-    const char* filename = argv[1]; // Путь к текстовому файлу
+    const char* filename = argv[1]; 
     FILE* file;
     int n;
 
-    // Ввод количества чисел с проверкой
     n = safe_input_int("Введите количество чисел: ");
 
     int* numbers = (int*)malloc(n * sizeof(int));
@@ -80,13 +73,11 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Ввод чисел с проверкой
     printf("Введите %d целых чисел:\n", n);
     for (int i = 0; i < n; i++) {
         numbers[i] = safe_input_int("");
     }
 
-    // Открываем файл для записи
     errno_t err = fopen_s(&file, filename, "wb");
     if (err != 0) {
         printf("Ошибка открытия файла для записи!\n");
@@ -94,21 +85,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Записываем числа в файл
     for (int i = 0; i < n; i++) {
-        fprintf(file, "%d\n", numbers[i]); // Каждое число на новой строке
+        fprintf(file, "%d\n", numbers[i]); 
     }
     fclose(file);
 
-    // Открываем файл для чтения
     err = fopen_s(&file, filename, "rb");
     if (err != 0) {
         printf("Ошибка открытия файла для чтения!\n");
         free(numbers);
         return 1;
     }
-
-    // Считываем числа из файла
+    
     for (int i = 0; i < n; i++) {
         fscanf_s(file, "%d", &numbers[i]);
     }
@@ -117,7 +105,6 @@ int main(int argc, char* argv[]) {
     printf("Числа из файла:\n");
     print_array(numbers, n);
 
-    // Подсчет количества простых чисел
     int prime_count = 0;
     for (int i = 0; i < n; i++) {
         if (is_prime(numbers[i])) {
@@ -126,10 +113,8 @@ int main(int argc, char* argv[]) {
     }
     printf("Количество простых чисел: %d\n", prime_count);
 
-    // Замена знака каждого числа на противоположный
     negate_numbers(numbers, n);
 
-    // Открываем файл для записи
     err = fopen_s(&file, filename, "wb");
     if (err != 0) {
         printf("Ошибка открытия файла для записи!\n");
@@ -137,7 +122,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Записываем измененные числа в файл
     for (int i = 0; i < n; i++) {
         fprintf(file, "%d\n", numbers[i]);
     }
@@ -146,10 +130,8 @@ int main(int argc, char* argv[]) {
     printf("Числа после замены знака:\n");
     print_array(numbers, n);
 
-    // Сортировка отрицательных чисел по убыванию
     sort_negatives(numbers, n);
 
-    // Открываем файл для записи
     err = fopen_s(&file, filename, "wb");
     if (err != 0) {
         printf("Ошибка открытия файла для записи!\n");
@@ -157,7 +139,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Записываем отсортированные числа в файл
     for (int i = 0; i < n; i++) {
         fprintf(file, "%d\n", numbers[i]);
     }
